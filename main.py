61a5977f57b9aaa3a019ababbc7981decf70ca6c
void4 <arndtschnabel@freenet.de>
2020-12-03 00:38:24 +0100

    Reset with r

--- main.py
+++ main.py
@@ -17,12 +17,19 @@ from battle import *
 RED = (255,0,0)
 BLUE = (0,0,255)
 
-v1 = Virus(RED, [randint(0,NUM_INSTR-1) for i in range(10)])
-v2 = Virus(BLUE, [randint(0,NUM_INSTR-1) for i in range(10)])
-
 SIZE = 128
 
-battle = Battle(2, SIZE, [((16,16), v1), ((SIZE-16,SIZE-16), v2)])
+battle = None
+
+def init_battle():
+	global battle
+
+	v1 = Virus(RED, [randint(0,NUM_INSTR-1) for i in range(10)])
+	v2 = Virus(BLUE, [randint(0,NUM_INSTR-1) for i in range(10)])
+
+	battle = Battle(2, SIZE, [((16,16), v1), ((SIZE-16,SIZE-16), v2)])
+
+init_battle()
 
 i = 0
 running = True
@@ -36,6 +43,9 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_r:
+				init_battle()
 
 	pygame.display.flip()
 	sleep(0.016)
